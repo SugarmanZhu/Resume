c2ae985cb1e5ddbd0dbfda79e7470543307d71e7
Xiaotan Zhu <sugarmanzhu@gmail.com>
2024-05-07 07:45:22 +0100

    resume v1

---
+++ pages/index.vue
@@ -0,0 +1,134 @@
+<template>
+    <div class="bg-white w-a4w h-a4h shadow-lg print:shadow-none mx-auto my-6 print:m-0">
+        <div class="px-8 py-4 flex bg-sky-50">
+            <div class="flex-grow space-y-1 my-4">
+                <div class="text-5xl font-bold uppercase">Xiaotan Zhu</div>
+                <div class="text-xl font-semibold">Software Engineer</div>
+            </div>
+            <div class="space-y-2 my-auto">
+                <div class="text-right text-base">
+                    <div class="flex space-x-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 rounded-full bg-black text-white p-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
+                        </svg>
+                        <div>
+                            <a href="mailto:sugarmanzhu@gmail.com">sugarmanzhu@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-right text-base">
+                    <div class="flex space-x-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 rounded-full bg-black text-white p-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                        </svg>
+                        <div>
+                            <a href="https://github.com/sugarmanzhu" target="_blank">github.com/sugarmanzhu</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-right text-base">
+                    <div class="flex space-x-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 rounded-full bg-black text-white p-1">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+                        </svg>
+                        <div>
+                            <a href="tel:+447410078526">+44 7410 078526</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mx-8 my-4 flex flex-col space-y-4">
+            <div>
+                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
+                    Summary of qualifications
+                </div>
+                <div class="m-2 pl-6">
+                    <ul class="list-outside list-disc">
+                        <li>MSc in <b>Computer Science</b></li>
+                        <li>Experienced in <b>TypeScript, JavaScrip, React</b></li>
+                        <li>Skilled in <b>HTML, Tailwind CSS, Web Components</b></li>
+                        <li>Experienced in implementing <b>asynchronous</b> JavaScript operations and <b>API data fetching</b></li>
+                        <li>Skilled in processing and integrating <b>JSON</b> and other data formats into web applications</li>
+                        <li>Proficient in Crafting <b>User-Friendly, Mobile-Responsive</b> HTML and CSS</li>
+                        <li>Proactively Enhances Codebase and Developer Experience</li>
+                        <li>Attentive to UI Details</li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div>
+                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
+                    Experience
+                </div>
+                <div class="flex m-2">
+                    <div class="flex-grow flex flex-row space-x-1.5">
+                        <div class="font-semibold text-xl">Pharmica</div>
+                        <div class="h-3/5 mt-1.5 w-0.5 bg-sky-300 rounded-full"></div>
+                        <div class="font-semibold text-xl text-gray-700">Full Stack Software Engineer</div>
+                        <div class="h-3/5 mt-1.5 w-0.5 bg-sky-300 rounded-full"></div>
+                        <div class="font-semibold text-xl text-gray-500">London</div>
+                    </div>
+                    <div class="grid content-end text-gray-500">May 2023 - Present</div>
+                </div>
+                <div class="m-2 pl-6">
+                    <ul class="list-outside list-disc">
+                        <li>Redesigned Web Interfaces for Enhanced Usability: Revamped web pages to <b>enhance user experience, improved layout, user accessibility and mobile responsiveness.</b></li>
+                        <li>Enhanced Payment Systems: Integrated and optimized multiple payment solutions including Stripe, PayPal, and Apple Pay. Improved <b>error handling, enhanced security measures, and reduced processing times</b>, resulting in a smoother transaction experience for users.</li>
+                        <li>Optimized Delivery Systems: Implemented Royal Mail <b>API</b> to automatically generate thousands of labels daily and integrated tracking features to monitor delivery status, significantly enhancing operational efficiency and customer service.</li>
+                        <li>Data Analysis: Utilized <b>SQL</b> to extract data from RDS for comprehensive analysis, assessing the effectiveness of changes, advertising campaigns, and <b>A/B testing</b>. Additionally analyzed profit data to inform strategic decision-making.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
+                    Certifications
+                </div>
+                <div class="m-2 pl-6">
+                    <ul class="list-outside list-disc">
+                        <li><a href="https://xiaotanzhupublic.s3.eu-west-2.amazonaws.com/AWS+Certified+Cloud+Practitioner+certificate.pdf" target="_blank"><b>AWS</b> Certified Cloud Practitioner</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
+                    Education
+                </div>
+                <div class="m-2 flex flex-col space-y-1">
+                    <div class="flex">
+                        <div class="flex-grow">
+                            <div class="font-semibold text-xl">Imperial College London</div>
+                            <div class="text-gray-700">MSc in Advanced Computing</div>
+                        </div>
+                        <div class="grid content-end text-gray-500">Oct 2021 - Oct 2022</div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-grow">
+                            <div class="font-semibold text-xl">The University of Manchester</div>
+                            <div class="text-gray-700">BSc in Computer Science</div>
+                        </div>
+                        <div class="grid content-end text-gray-500">Sep 2018 - Jul 2021</div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-grow">
+                            <div class="font-semibold text-xl">Dulwich International High School Suzhou</div>
+                            <div class="text-gray-700">A Level</div>
+                        </div>
+                        <div class="grid content-end text-gray-500">Sep 2014 - Jul 2018</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        spacing: {
+          'a4w': '794px', // A4 width in pixels
+          'a4h': '1123px' // A4 height in pixels
+        }
+      }
+    }
+  }
