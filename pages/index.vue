741a35f728ecd7f295eb321375fde10f10b2ae4c
Xiaotan Zhu <sugarmanzhu@gmail.com>
2024-05-26 01:12:21 +0100

    v2

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,82 @@
 <template>
     <div class="bg-white w-a4w h-a4h shadow-lg print:shadow-none mx-auto my-6 print:m-0">
-        <div class="px-8 py-4 flex bg-sky-50">
+        <div class="px-8 py-4 flex bg-sky-100">
             <div class="flex-grow space-y-1 my-4">
-                <div class="text-5xl font-bold uppercase">Xiaotan Zhu</div>
-                <div class="text-xl font-semibold">Software Engineer</div>
+                <div class="text-5xl font-bold">Xiaotan Zhu</div>
+                <div class="text-xl font-semibold text-gray-700">Software Engineer</div>
             </div>
             <div class="space-y-2 my-auto">
                 <div class="text-right text-base">
-                    <div class="flex space-x-1">
+                    <div class="flex space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6 rounded-full bg-black text-white p-1">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
                         </svg>
                         <div>
-                            <a href="mailto:sugarmanzhu@gmail.com">sugarmanzhu@gmail.com</a>
+                            <a target="_blank" href="mailto:sugarmanzhu@gmail.com">sugarmanzhu@gmail.com</a>
                         </div>
                     </div>
                 </div>
                 <div class="text-right text-base">
-                    <div class="flex space-x-1">
+                    <div class="flex space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6 rounded-full bg-black text-white p-1">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                         </svg>
                         <div>
-                            <a href="https://github.com/sugarmanzhu" target="_blank">github.com/sugarmanzhu</a>
+                            <a target="_blank" href="https://github.com/sugarmanzhu">github.com/sugarmanzhu</a>
                         </div>
                     </div>
                 </div>
                 <div class="text-right text-base">
-                    <div class="flex space-x-1">
+                    <div class="flex space-x-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6 rounded-full bg-black text-white p-1">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                         </svg>
                         <div>
-                            <a href="tel:+447410078526">+44 7410 078526</a>
+                            <a target="_blank" href="tel:+447410078526">+44 7410 078526</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="mx-8 my-4 flex flex-col space-y-4">
+        <div class="mx-8 my-4 flex flex-col space-y-3">
             <div>
-                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
-                    Summary of qualifications
+                <div class="bg-sky-100 w-full text-xl font-bold px-3 py-1 rounded-md drop-shadow-sm flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
+                    </svg>
+                    <div>Summary of qualifications</div>
                 </div>
                 <div class="m-2 pl-6">
                     <ul class="list-outside list-disc">
                         <li>MSc in <b>Computer Science</b></li>
-                        <li>Experienced in <b>TypeScript, JavaScrip, React</b></li>
+                        <li>Experienced in <b>TypeScript, JavaScript, React</b></li>
                         <li>Skilled in <b>HTML, Tailwind CSS, Web Components</b></li>
-                        <li>Experienced in implementing <b>asynchronous</b> JavaScript operations and <b>API data fetching</b></li>
-                        <li>Skilled in processing and integrating <b>JSON</b> and other data formats into web applications</li>
+                        <li>Experienced in implementing <b>asynchronous</b> JavaScript operations and <b>API data
+                                fetching</b></li>
+                        <li>Skilled in processing and integrating <b>JSON</b> and other data formats into web
+                            applications</li>
                         <li>Proficient in Crafting <b>User-Friendly, Mobile-Responsive</b> HTML and CSS</li>
-                        <li>Proactively Enhances Codebase and Developer Experience</li>
-                        <li>Attention to UI Details</li>
+                        <!-- <li>Proactively Enhances Codebase and Developer Experience</li>
+                        <li>Attention to UI Details</li> -->
                     </ul>
                 </div>
             </div>
-            
+
             <div>
-                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
-                    Experience
+                <div class="bg-sky-100 w-full text-xl font-bold px-3 py-1 rounded-md drop-shadow-sm flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                    </svg>
+                    <div>Experience</div>
                 </div>
                 <div class="flex m-2">
                     <div class="flex-grow flex flex-row space-x-1.5">
@@ -80,33 +90,79 @@
                 </div>
                 <div class="m-2 pl-6">
                     <ul class="list-outside list-disc">
-                        <li>Redesigned Web Interfaces for Enhanced Usability: Revamped web pages to <b>enhance user experience, improved layout, user accessibility and mobile responsiveness.</b></li>
-                        <li>Enhanced Payment Systems: Integrated and optimized multiple payment solutions including Stripe, PayPal, and Apple Pay. Introduced a robust subscription system and utilized Stripe <b>webhooks for real-time payment status updates</b>, which improved <b>error handling, enhanced security measures, and reduced processing times</b>.</li>
-                        <li>Optimized Delivery Systems: Implemented Royal Mail <b>API</b> to <b>automatically generate thousands of labels</b> daily and integrated tracking features to monitor delivery status, significantly enhancing operational efficiency and customer service.</li>
-                        <li>Data Analysis: Utilized <b>SQL</b> to extract data from <b>RDS</b> for comprehensive analysis, assessing the effectiveness of advertising campaigns, and <b>A/B testing</b>.</li>
+                        <li>Redesigned Web Interfaces for Enhanced Usability: Revamped web pages to <b>enhance user
+                                experience, improved layout, user accessibility and mobile responsiveness.</b></li>
+                        <li>Enhanced Payment Systems: Integrated and optimized multiple payment solutions including
+                            Stripe, PayPal, and Apple Pay. Introduced a robust subscription system and utilized Stripe
+                            <b>webhooks for real-time payment status updates</b>, which improved <b>error handling,
+                                enhanced security measures, and reduced processing times</b>.
+                        </li>
+                        <li>Optimized Delivery Systems: Implemented Royal Mail <b>API</b> to <b>automatically generate
+                                thousands of labels</b> daily and integrated tracking features to monitor delivery
+                            status, significantly enhancing operational efficiency and customer service.</li>
+                        <li>Data Analysis: Utilized <b>SQL</b> to extract data from <b>RDS</b> for comprehensive
+                            analysis, assessing the effectiveness of advertising campaigns, and <b>A/B testing</b>.</li>
                     </ul>
                 </div>
             </div>
-
+            
             <div>
-                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
-                    Certifications
+                <div class="bg-sky-100 w-full text-xl font-bold px-3 py-1 rounded-md drop-shadow-sm flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-700">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                    </svg>
+                    <div>Projects</div>
                 </div>
                 <div class="m-2 pl-6">
                     <ul class="list-outside list-disc">
-                        <li><a href="https://xiaotanzhupublic.s3.eu-west-2.amazonaws.com/AWS+Certified+Cloud+Practitioner+certificate.pdf" target="_blank"><b>AWS</b> Certified Cloud Practitioner</a></li>
+                        <li>
+                            <span>Xiaotanology: Built a fake e-commerce platform created using <b>Nuxt.js</b>.</span>
+                            <a target="_blank" href="https://xiaotanology.zhuxiaotan.com" class="mx-2 underline">View website
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 inline">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <span>Deep Q-Learning Project: Implemented <b>reinforcement learning</b> techniques to develop an agent capable of playing Super Mario Bros at human level using <b>only pixel input</b>.</span>
+                            <a target="_blank" href="https://xiaotanzhupublic.s3.eu-west-2.amazonaws.com/AI+plays+Mario.gif" class="mx-2 underline">View sample run
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 inline">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                </svg>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
 
             <div>
-                <div class="bg-sky-50 w-full uppercase text-2xl font-bold px-2 py-1">
-                    Education
+                <div class="bg-sky-100 w-full text-xl font-bold px-3 py-1 rounded-md drop-shadow-sm flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                    </svg>
+                    <div>Certification</div>
+                </div>
+                <div class="m-2 pl-6">
+                    <ul class="list-outside list-disc">
+                        <li>
+                            <a target="_blank" href="https://xiaotanzhupublic.s3.eu-west-2.amazonaws.com/AWS+Certified+Cloud+Practitioner+certificate.pdf"><b>AWS</b> Certified Cloud Practitioner</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div class="bg-sky-100 w-full text-xl font-bold px-3 py-1 rounded-md drop-shadow-sm flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-gray-700">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                    </svg>
+                    <div>Education</div>
                 </div>
                 <div class="m-2 flex flex-col space-y-1">
                     <div class="flex">
                         <div class="flex-grow">
-                            <div class="font-semibold text-xl">Imperial College London</div>
+                            <div class="font-semibold">Imperial College London</div>
                             <div class="text-gray-700">MSc in Advanced Computing</div>
                         </div>
                         <div class="grid content-end text-gray-500">Oct 2021 - Oct 2022</div>
@@ -114,19 +170,19 @@
 
                     <div class="flex">
                         <div class="flex-grow">
-                            <div class="font-semibold text-xl">The University of Manchester</div>
+                            <div class="font-semibold">The University of Manchester</div>
                             <div class="text-gray-700">BSc in Computer Science</div>
                         </div>
                         <div class="grid content-end text-gray-500">Sep 2018 - Jul 2021</div>
                     </div>
 
-                    <div class="flex">
+                    <!-- <div class="flex">
                         <div class="flex-grow">
-                            <div class="font-semibold text-xl">Dulwich International High School Suzhou</div>
+                            <div class="font-semibold">Dulwich International High School Suzhou</div>
                             <div class="text-gray-700">A Level</div>
                         </div>
                         <div class="grid content-end text-gray-500">Sep 2014 - Jul 2018</div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
